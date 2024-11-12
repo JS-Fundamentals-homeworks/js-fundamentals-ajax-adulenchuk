8778219905d2c6a40c0c0f7ba7336b1e5a201fef
adulenchuk <andrii.dulenchuk@gmail.com>
2024-11-12 22:04:23 +0200

    tasks done

--- task1.js
+++ task1.js
@@ -5,3 +5,11 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+const api = 'https://jsonplaceholder.typicode.com/users';
+const usersList = document.querySelector('ul.usersList')
+fetch(api).then(res => res.json()).then(data => {
+    for (const element of data) {
+        usersList.innerHTML += `<li>${element.name}</li>`;
+    }
+});
--- task2.js
+++ task2.js
@@ -5,3 +5,19 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+let input = document.getElementById('userNameInput');
+const button = document.getElementById('getUserButton');
+let userCity = document.getElementById('userCity');
+
+function getCity() {
+    fetch(api).then(res => res.json()).then(data => {
+        data.forEach(element => {
+            if (element.name === input.value) {
+                userCity.textContent = element.address.city;
+            }
+        });
+    });    
+}
+
+button.onclick = getCity;
